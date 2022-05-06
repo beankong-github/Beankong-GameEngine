ccdeaa76bc7b453a5a371fcc336714a985fa0e87
YooSubeen <subeenyoo1002@gmail.com>
2022-05-07 02:15:08 +0900

    CResource 구현 (2022-05-07)

--- Beankong-GameEngine/Project/Engine/CResource.h
+++ Beankong-GameEngine/Project/Engine/CResource.h
@@ -1,11 +1,38 @@
 #pragma once
 #include "CEntity.h"
 
+#include "ResMgr.h"
+
 class CResource :
     public CEntity
 {
 private:
     wstring m_strKey;
-    wstring m_strRel
+    wstring m_strRelativePath;
+    UINT    m_iRefCount;
+
+protected:  
+    void SetKey(const wstring& _strKey)             { m_strKey = _strKey; }
+    void SetRelativePath(const wstring& _strPath)   { m_strRelativePath = _strPath; }
+    bool CheckFail(HRESULT _hr);
+
+public:
+    const wstring& GetKey()             { return m_strKey; }
+    const wstring& GetRelativePath()    { return m_strRelativePath; }
+
+protected:
+    virtual int Load(const wstring& _strFilepath) = 0;
+    CLONE_DISABLE(CResource)
+
+private:
+    void AddRef() { ++m_iRefCount; }
+    void SubRef() { --m_iRefCount; }
+
+public:
+    CResource();
+    virtual ~CResource();
+
+    friend class ResMgr;
+
 };
 
