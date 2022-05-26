c7d9bb1b99964f248d3c80bde588e09880d37a24
YooSubeen <93888320+beankong-github@users.noreply.github.com>
2022-05-26 18:20:09 +0900

    정점 입력 구조 생성 함수 추가 (2022-05-26)

--- Beankong-GameEngine/Project/Engine/ResMgr_init.cpp
+++ Beankong-GameEngine/Project/Engine/ResMgr_init.cpp
@@ -64,4 +64,38 @@ void ResMgr::CreateEngineTexture()
 
 void ResMgr::MakeInputLayoutInfo()
 {
+	// 정점 입력 구조 (Input Layout 생성)
+	UINT iOffset = 0;
+	D3D11_INPUT_ELEMENT_DESC tInputDesc = {};
+
+	tInputDesc.SemanticName = "POSITION";	// Semantic 이름
+	tInputDesc.SemanticIndex = 0;			// 중복 이름인 경우 인덱스로 구분
+	tInputDesc.InputSlot = 0;
+	tInputDesc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	tInputDesc.InstanceDataStepRate = 0;
+	tInputDesc.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	tInputDesc.AlignedByteOffset = iOffset;	// Vertex의 시작 주소로부터 해당 정보가 있는 위치(in bytes)
+	iOffset += 12;	// Vec3 크기
+
+	CGraphicsShader::AddInputLayout(tInputDesc);
+
+	tInputDesc.SemanticName = "COLOR";
+	tInputDesc.SemanticIndex = 0;
+	tInputDesc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	tInputDesc.InstanceDataStepRate = 0;
+	tInputDesc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	tInputDesc.AlignedByteOffset = iOffset;
+	iOffset += 16;	// Vec4 크기
+
+	CGraphicsShader::AddInputLayout(tInputDesc);
+
+	tInputDesc.SemanticName = "TEXCOORD";
+	tInputDesc.SemanticIndex = 0;
+	tInputDesc.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	tInputDesc.InstanceDataStepRate = 0;
+	tInputDesc.Format = DXGI_FORMAT_R32G32_FLOAT;
+	tInputDesc.AlignedByteOffset = iOffset;
+	iOffset += 8;	// Vec2 크기
+
+	CGraphicsShader::AddInputLayout(tInputDesc);
 }
